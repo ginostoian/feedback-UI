8ea84a8df30a4d3e0fecd8a0c698ebd22fb20a4a
Gino Stoian <ginostoian@gmail.com>
2022-05-01 00:26:22 +0100

    add fake backend with json server and add API calls

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,27 +1,47 @@
-import { v4 as uuidv4 } from 'uuid'
-
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This feedback is from context',
-            rating: 10
-        }
-    ])
+    const [feedback, setFeedback] = useState([])
 
-    function handleDelete(id) {
-        console.log(id)
+    // Fetch feedback from Json Server
+    const fetchFeedback = async () => {
+        const response = await fetch('/feedback?_sort=id&_order=desc', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            }
+
+        })
+        const data = await response.json()
+        setFeedback(data)
+    }
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    async function handleDelete(id) {
+        await fetch(`/feedback/${id}`, {
+            method: 'DELETE'
+        })
+
         const newFeedback = feedback.filter(item => item.id !== id)
         setFeedback(newFeedback)
     }
 
-    function addFeedback(newFeedback) {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    async function addFeedback(newFeedback) {
+        const response = await fetch('/feedback', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newFeedback)
+        })
+
+        const data = await response.json()
+        setFeedback([data, ...feedback])
     }
 
     return <FeedbackContext.Provider value={{
